438e072765df2bfa2c635ac1b96fffc28951c217
Jose Narvaez <jose.narvaez.ie@gmail.com>
2020-06-14 13:12:01 -0500

    Initial commit for eurekaServer

---
+++ src/test/java/com/sfg/brewery/eureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.brewery.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
